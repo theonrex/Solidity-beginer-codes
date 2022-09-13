eb3d3225b535de5d4c22f3fd83b412a21eb3c141
Olawande Bamidele <91835162+theonrex@users.noreply.github.com>
2022-09-13 10:58:00 +0100

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -20,3 +20,27 @@ constructor() public {
         count = count + 1;
     }
 }
+
+// Example Two
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter{
+    // all code goes here
+    uint public count = 5; //means the count value cant be negative // also means it cant be a string or array but only unit
+
+//public modifier means it can be called within and outside the contract
+//view function 
+//This is a read function meaning it just reads and get info from the blockchain // free
+  
+    //write function is a function that changes the state of the blockchain // cost gas
+    function increment() public{
+        count ++;
+    }
+    function decreament() public{
+        count = count -5 ;
+    }
+    function multiply() public{
+        count = count * 5;
+    }
+}
